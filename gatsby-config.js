46acab311c05a0e3501617dafeef14cb5edfeeea
chistopher KENNY <chistopherkenny@MacBook-Air-de-chistopher.local>
2022-01-29 14:22:51 +0100

    Finished Tut part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `My First Site`,
-        siteUrl: `https://www.yourdomain.tld`,
+        title: "My Super Cool Blog",
     },
     plugins: [
-
-    ]
-}
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog/`,
+            },
+        },
+        "gatsby-plugin-mdx",
+    ],
+};
